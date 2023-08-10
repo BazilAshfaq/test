725701307e78313a9a6c3fe681384ef43baf0dc8
BazilAshfaq <141966273+BazilAshfaq@users.noreply.github.com>
2023-08-10 18:33:49 -0400

    2nd modified account.java

--- ver2/Account.java
+++ ver2/Account.java
@@ -1,14 +1,12 @@
 package ver2;
 
 public class Account {
+	// New comment for balance
 	// Instance variable, only available inside this class.
-	private double balance;
+	private double balance; 
 	private String name;
 
-	// Constructor
-	public Account(double balance) {
-		this.balance = balance;
-	}
+
 
 	public Account(String name, double balance) {
 		this.name = name;
